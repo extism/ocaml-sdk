3a5ca56229476c2110a09aaa07a37017729498cc
zach <zach@dylibso.com>
2024-11-25 09:41:16 -0800

    chore: update uuidm

--- src/plugin.ml
+++ src/plugin.ml
@@ -204,7 +204,7 @@ let id { pointer; _ } =
   else
     let id = Bindings.extism_plugin_id pointer in
     let s = Ctypes.string_from_ptr id ~length:16 in
-    Uuidm.unsafe_of_bytes s
+    Uuidm.unsafe_of_binary_string s
 
 let reset { pointer; _ } = Bindings.extism_plugin_reset pointer
 
